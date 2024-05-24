c7473a50fe16dd97ed9d5c34c5af80b601f925c7
VanDuyHowl <160881058+VanDuyHowl@users.noreply.github.com>
2024-05-24 09:46:15 +0700

    Update QLHS.java

--- fita/vnua/bai3/QLHS.java
+++ fita/vnua/bai3/QLHS.java
@@ -17,26 +17,23 @@ public class QLHS {
 		
 	}
 	
-	int n;
 	public void nhapDanhSachHS(Scanner sc) {
 		
 		System.out.println("So luong hoc sinh co trong danh sach la: ");
-		n = sc.nextInt();
+		int n = sc.nextInt();sc.nextLine();
 		
 		System.out.println("Nhap thon tin hoc sinh: ");
 		for(int i=0;i<n;i++) {
 			System.out.println("Hoc sinh thu " + (i+1) + " la: ");
 			
 			HocSinh hs = new HocSinh();
-			hs.nhapThongTin(sc);
-			
-			
+			hs.nhapThongTin(sc);	
 			themHocSinh(hs);
 		}
 	}
 	
 	public void inDanhSach() {
-		n=dshs.size();
+		int n=dshs.size();
 		for(int i=0;i<n;i++) {
 			System.out.println("Hoc sinh thu " + (i+1) + ":");
 			dshs.get(i).inThongTin();
@@ -46,18 +43,28 @@ public class QLHS {
 	}
 	
 	public void timKiemHocSinh(int namSinh, String queQuan){
+		System.out.println("Danh sach hoc sinh co nam sinh "+ namSinh + "va que o "+"Thai nguyen"+":");
 		for (HocSinh hs : dshs) {
-			//if((hs.getngaySinh().getYear() + 1900 == namSinh) &&(queQuan.equals(getqueQuan())))
+			if((namSinh == hs.ngaySinh.getYear() + 1900) && (queQuan.equals(hs.queQuan)))
 				hs.inThongTin();
-		}
-		
+			
+		}	
+	}
+	
+	public void timKiemHocSinh(String lop){
+		System.out.println("Danh sach hoc sinh co lop "+ lop +":");
+		for (HocSinh hs : dshs) {
+			if((lop.equals(hs.getLop())))
+				
+				hs.inThongTin();
+		}	
 	}
 	public static void main(String[] args){
 		QLHS qlhs = new QLHS();
 		Scanner sc = new Scanner(System.in);
 		qlhs.nhapDanhSachHS(sc);
 		qlhs.timKiemHocSinh(1985, "Thai Nguyen");
-		qlhs.timKiemHocSinh(1985,"10A1");
+		qlhs.timKiemHocSinh("10A1");
 		}
 	
 	
