0b828a711fd2496181e1369882db1a74cbab3dfe
VanDuyHowl <160881058+VanDuyHowl@users.noreply.github.com>
2024-05-28 21:35:34 +0700

    Add files via upload

---
+++ Code_Java/Java_Core/src/fita/DeThiMau/Test.java
@@ -0,0 +1,17 @@
+package DeThiMau;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student stu = new Student("Nguyen Thanh Nam", "Trau Quy - Gia Lam");
+		stu.addCourseGrade("Java", 9);
+		stu.addCourseGrade("CSDL", 8);
+		stu.addCourseGrade("Pascal", 10);
+		stu.addCourseGrade("Network", 6);
+		
+		System.out.println(stu.toString());
+		System.out.println("Điểm trung bình môn là: "+stu.getAverageGrades());
+	}
+
+}
